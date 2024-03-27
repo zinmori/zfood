7261b9502ca1b9fc96c7d44c474f8190cc3e1e04
Ezechiel AGBAN <ezechiel9087@gmail.com>
2024-03-27 16:24:59 +0000

    image without link

--- src/components/MealItem.jsx
+++ src/components/MealItem.jsx
@@ -12,10 +12,7 @@ export default function MealItem({ meal }) {
   return (
     <li className="meal-item">
       <article>
-        <img
-          src={`https://foodbackend-z.vercel.app/${meal.image}`}
-          alt={meal.name}
-        />
+        <img src={meal.image} alt={meal.name} />
         <div>
           <h3>{meal.name}</h3>
           <p className="meal-item-price">
